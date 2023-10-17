c5617fbee18cae059ca47d2dc9f7adc22a62c54c
username <Jzazzi56@gmail.comn>
2023-10-16 17:06:49 -0700

    changed port

--- src/app/(dashboard)/blog/page.jsx
+++ src/app/(dashboard)/blog/page.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch("http://localhost:7090/api/posts", {
     cache: "no-store",
   });
 

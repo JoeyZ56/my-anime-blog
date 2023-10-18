0d76a21c5cef04278916144fee4666f1ee8c4621
username <Jzazzi56@gmail.comn>
2023-10-18 09:05:20 -0700

    trying to fix post id fetch request

--- src/app/(dashboard)/blog/[id]/page.jsx
+++ src/app/(dashboard)/blog/[id]/page.jsx
@@ -1,32 +1,60 @@
+"use client";
 import React from "react";
 import styles from "./postId.module.scss";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { notFound } from "next/navigation";
-import Link from "next/link";
+// import Link from "next/link";
 
 async function getData(id) {
-  const res = await fetch(`api/posts/${id}`, {
-    cache: "no-store",
-  });
+  console.log("Fetching data for ID:", id);
+  try {
+    const res = await fetch(`http://localhost:7090/api/posts/${id}`, {
+      cache: "no-store",
+    });
 
-  if (!res.ok) {
+    if (!res.ok) {
+      console.error(`Failed to fetch data for ID ${id}. Status: ${res.status}`);
+      throw new Error(`Failed to fetch data for ID ${id}`);
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("Error in getData:", error);
     return notFound();
   }
-
-  return res.json();
 }
 
 //use generate  meta data for dynamic pages
-export async function generateMetadata({ params }) {
-  const post = await getData(params.id);
-  return {
-    title: post.title,
-    description: post.desc,
-  };
-}
+// export async function generateMetadata({ params }) {
+//   const post = await getData(params.id);
+//   return {
+//     title: post.title,
+//     description: post.desc,
+//   };
+// }
 
-const BlogPost = async ({ params }) => {
-  const data = await getData(params.id);
+const BlogPost = ({ params }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("Fetching data for ID:", params.id);
+        const postData = await getData(params.id);
+        console.log("Received data:", postData);
+        setData(postData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [params.id]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={styles.container} id="postsinfo">

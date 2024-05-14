6d6f5e0779a4944320c621008409487afb537100
username <Jzazzi56@gmail.com>
2024-05-13 21:22:23 -0700

    created modal and text area for bio on account page

--- src/app/(dashboard)/account/page.jsx
+++ src/app/(dashboard)/account/page.jsx
@@ -1,16 +1,31 @@
 "use client";
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import styles from "./account.module.scss";
 import Image from "next/image";
+import Modal from "@/components/Modal/Modal";
 
 const Account = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const { data: session } = useSession();
 
   const user = session?.user;
 
+  const handleBioModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const saveBio = () => {
+    // save bio to db
+  };
+
   return (
     <div id="account-container">
-      <h1>Account</h1>
+      {/* <h1>{user.name}s account</h1> */}
       <div>
         <Image
           src="https://tse1.mm.bing.net/th?id=OIP.8nDOOlaYuDpSUAFEG0xKPgHaFy&pid=Api&P=0&h=180"
@@ -20,11 +35,27 @@ const Account = () => {
           style={{ borderRadius: "50%" }}
         />
       </div>
-      <div>
-        <h2>{user?.name}</h2>
-        {/* <h3>{user.email}</h3>
-        <h3>{user.bio}</h3> */}
-      </div>
+
+      {/* <p>{user.bio}</p> */}
+      <button onClick={handleBioModal}>Edit Bio</button>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <h2>Edit Bio</h2>
+
+        <textarea
+          input="text"
+          style={{
+            width: "100%",
+            height: "100px",
+            padding: "10px",
+            margin: "10px 0",
+            boxSizing: "border-box",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <button onClick={() => null}>Save</button>
+        <button onClick={closeModal}>Close</button>
+      </Modal>
     </div>
   );
 };
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,18 @@
+import styles from "./Modal.module.scss";
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!open) return null;
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.closeButton} onClick={onClose}>
+          &times;
+        </button>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;

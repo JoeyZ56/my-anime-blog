a4aebc8da23afed9fbd3df0f2c717d747fb645ea
username <Jzazzi56@gmail.com>
2024-05-13 20:55:06 -0700

    got the information to pull currectly

--- src/app/(dashboard)/account/page.jsx
+++ src/app/(dashboard)/account/page.jsx
@@ -1,8 +1,31 @@
+"use client";
+import { useSession } from "next-auth/react";
 import styles from "./account.module.scss";
+import Image from "next/image";
 
-const Account = ({ getUser }) => {
+const Account = () => {
+  const { data: session } = useSession();
+
+  const user = session?.user;
+
   return (
-    <div className="account-container">{/* <h1>{getUser.name}</h1> */}</div>
+    <div id="account-container">
+      <h1>Account</h1>
+      <div>
+        <Image
+          src="https://tse1.mm.bing.net/th?id=OIP.8nDOOlaYuDpSUAFEG0xKPgHaFy&pid=Api&P=0&h=180"
+          alt="user"
+          width={200}
+          height={200}
+          style={{ borderRadius: "50%" }}
+        />
+      </div>
+      <div>
+        <h2>{user?.name}</h2>
+        {/* <h3>{user.email}</h3>
+        <h3>{user.bio}</h3> */}
+      </div>
+    </div>
   );
 };
 
